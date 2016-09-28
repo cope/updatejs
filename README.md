3b04139fbdc88a86e46e9d961c782fa1c604bd92
Predrag Stojadinovic <predrags@nvidia.com>
2016-09-28 11:58:42 +0200

    adding:  - recursive checking of all subfolders  - i/u/p options to select particular calls individually

--- README.md
+++ README.md
@@ -6,25 +6,57 @@ The script will check for package.json and bower.json before running install/upd
 
 The script will recursively search for the location of bower.json in order to handle special "client" folders.
 
-#### Install:
+## Install:
     npm i -g updatejs
 
-#### Usage:
+## Options:
+    -h, --help       output usage information
+    -V, --version    output the version number
+
+    -n, --npm        Perform *only* npm calls
+    -b, --bower      Perform *only* bower calls
+
+    -i, --install    Perform *only* install calls
+    -u, --update     Perform *only* update calls
+    -p, --prune      Perform *only* prune calls
+
+    -r, --recursive  Perform calls recursively in all subfolders
+                     WARNING: The more projects you have, the slower the update will be...
+
+    <no params>      Same as using -nbiup:
+                     Performs all npm and bower install/update/prune calls
+
+## Usage:
     > update
+    For npm, currently in: C:\GIT\yourproject
     Running npm install...
+    - npm install done.
     Running npm update...
+    - npm update done.
     Running npm prune...
+    - npm prune done.
+    For bower, currently in: C:\GIT\yourproject
     Running bower install...
+    - bower install done.
     Running bower update...
+    - bower update done.
     Running bower prune...
+    - bower prune done.
     Done.
 
-##### Options:
-    -h, --help        output usage information
-    -V, --version     output the version number
-    
-    -n, --npm         Perform *only* npm install/update/prune calls
-    -b, --bower       Perform *only* bower install/update/prune calls
-    
-    -nb, <no params>  Perform both npm and bower install/update/prune calls
+### Additional examples: Only install
+    > update -i
+    For npm, currently in: C:\GIT\yourproject
+    Running npm install...
+    - npm install done.
+    For bower, currently in: C:\GIT\yourproject
+    Running bower install...
+    - bower install done.
+    Done.
 
+### Additional examples: Only npm install
+    > update -ni
+    For npm, currently in: C:\GIT\yourproject
+    Running npm install...
+    - npm install done.
+    Done.
--- update.js
+++ update.js
@@ -9,38 +9,75 @@
  * -h, --help        output usage information
  * -V, --version     output the version number
  *
- * -n, --npm         Perform *only* npm install/update/prune calls
- * -b, --bower       Perform *only* bower install/update/prune calls
+ * -n, --npm         Perform *only* npm calls
+ * -b, --bower       Perform *only* bower calls
  *
- * -nb, <no params>  Perform both npm and bower install/update/prune calls
+ * -i, --install     Perform *only* install calls
+ * -u, --update      Perform *only* update calls
+ * -p, --prune       Perform *only* prune calls
+ *
+ * -r, --recursive   Perform calls recursively in all subfolders
+ *                   WARNING: The more projects you have, the slower the update will be...
+ *
+ * <no params>       Same as using -nbiup:
+ *                   Performs all npm and bower install/update/prune calls
  */
 
 var fs = require("fs");
+var _ = require("lodash");
 var cmd = require('node-cmd');
 var colors = require('colors');
 var Promise = require('bluebird');
-var program = require('commander');
+var commander = require('commander');
 
-program
-.version('0.0.3')
+commander
+.version('0.1.0')
 .option('', '')
-.option('-n, --npm', 'Perform *only* npm install/update/prune calls', false)
-.option('-b, --bower', 'Perform *only* bower install/update/prune calls', false)
+.option('-n, --npm', 'Perform *only* npm calls', false)
+.option('-b, --bower', 'Perform *only* bower calls', false)
 .option('', '')
-// .option('-r, --recursive', 'Perform bower install/update/prune calls recursively')
-// .option('', 'Does not work with the -n option')
-// .option('', '')
-.option('-nb, <no params>', 'Perform both npm and bower install/update/prune calls')
-.parse(process.argv)
-;
+.option('-i, --install', 'Perform *only* install calls', false)
+.option('-u, --update', 'Perform *only* update calls', false)
+.option('-p, --prune', 'Perform *only* prune calls', false)
+.option('', '')
+.option('-r, --recursive', 'Perform calls recursively in all subfolders', false)
+.option('', 'WARNING: The more projects you have, the slower the update will be...')
+.option('', '')
+.option('<no params>', 'Same as using -nbiup:')
+.option('', 'Performs all npm and bower install/update/prune calls')
+.parse(process.argv);
 
-// If just `update` is executed, run npm and bower
-if (!program.npm && !program.bower) {
-	program.npm = true;
-	program.bower = true;
+commander.npm = true === commander.npm;
+commander.bower = true === commander.bower;
+commander.install = true === commander.install;
+commander.update = true === commander.update;
+commander.prune = true === commander.prune;
+commander.recursive = true === commander.recursive;
+
+// If just `update` is executed, run both npm and bower, and run all three iup calls
+if (!commander.npm && !commander.bower) {
+	commander.npm = commander.bower = true;
+}
+if (!commander.install && !commander.update && !commander.prune) {
+	commander.install = commander.update = commander.prune = true;
 }
 
+// commander debug:
+// console.log("commander.npm: " + commander.npm);
+// console.log("commander.bower: " + commander.bower);
+// console.log("commander.install: " + commander.install);
+// console.log("commander.update: " + commander.update);
+// console.log("commander.prune: " + commander.prune);
+// console.log("commander.recursive: " + commander.recursive);
+
 var update = {
+
+	goto: function (location, command) {
+		process.chdir(location);
+		console.log(("For " + command + ", currently in: " + location).red);
+		return Promise.defer().resolve(true);
+	},
+
 	run: function (command, msg1, msg2) {
 		var deferred = Promise.defer();
 		console.log(msg1);
@@ -51,107 +88,134 @@ var update = {
 		});
 		return deferred.promise;
 	},
+
 	seq: function (promises) {
 		return Promise.map(promises, function (promiseFn) {
-			return promiseFn(); //make sure that here You return Promise
+			return promiseFn();
 		}, {concurrency: 1}); //it will run promises sequentially
 	},
-	checkNPM: function () {
-		this.hasPackageJSON = false;
+
+	check: function (path, file) {
 		try {
-			var stats = fs.lstatSync(process.cwd() + '/package.json');
-			this.hasPackageJSON = stats.isFile();
-		}
-		catch (err) {
-		}
-	},
-	checkBower: function (path) {
-		try {
-			var stats = fs.lstatSync(path + '/bower.json');
+			var stats = fs.lstatSync(path + '/' + file);
 			return stats.isFile();
 		}
 		catch (err) {
 		}
 		return false;
 	},
+
 	isValidFolder: function (folder) {
 		return "node_modules" !== folder && "bower_components" !== folder;
 	},
-	findBower: function (path) {
-		if (this.checkBower(path)) {
-			this.bowerLocation = path;
-		} else {
-			var files = fs.readdirSync(path);
-			var file, id, x = 0, newName;
-			for (id in files) {
-				if (files.hasOwnProperty(id)) {
-					file = files[id];
-					var isDir = fs.lstatSync(path + "/" + file).isDirectory();
-					var isValid = this.isValidFolder(file);
-					if (isDir && isValid) {
-						if (this.checkBower(path + "/" + file)) {
-							this.bowerLocation = path + "/" + file;
-						} else {
-							!this.bowerLocation && this.findBower(path + "/" + file);
+
+	find: function (path, file, rootOnly) {
+		rootOnly = true === rootOnly && !commander.recursive;
+
+		var ret = [];
+		if (this.check(path, file)) {
+			ret.push(path);
+		}
+
+		var done = rootOnly || (!_.isEmpty(ret) && !commander.recursive);
+		if (!done) {
+			var id, child, children = fs.readdirSync(path);
+			for (id in children) {
+				if (children.hasOwnProperty(id)) {
+					child = children[id];
+					if (this.isValidFolder(child)) {
+						if (fs.lstatSync(path + "/" + child).isDirectory()) {
+							ret = ret.concat(this.find(path + "/" + child, file));
+
+							if (!_.isEmpty(ret) && !commander.recursive) {
+								break;
+							}
 						}
 					}
 				}
 			}
 		}
-	},
-	bower: function () {
-		this.bowerLocation = null;
-		this.findBower(process.cwd());
 
-		if (this.bowerLocation) {
-			process.chdir(this.bowerLocation);
+		return ret;
+	},
 
-			var self = this;
-			return [
-				function () {
-					return self.run('bower install', 'Running bower install...'.blue, 'Running bower install done.'.blue);
-				},
-				function () {
-					return self.run('bower update', 'Running bower update...'.blue, 'Running bower update done.'.blue);
-				},
-				function () {
-					return self.run('bower prune', 'Running bower prune...'.blue, 'Running bower prune done.'.blue);
-				}
-			];
-		} else {
-			console.log("No bower.json found, skipping bower calls.".underline.yellow);
-		}
-		return [];
+	npm: function (location) {
+		var self = this;
+		var promises = [
+			function () {
+				return self.goto(location, "npm");
+			}
+		];
+		commander.install && promises.push(function () {
+			return self.run('npm install', 'Running npm install...'.magenta, '- npm install done.'.magenta);
+		});
+		commander.update && promises.push(function () {
+			return self.run('npm update', 'Running npm update...'.magenta, '- npm update done.'.magenta);
+		});
+		commander.prune && promises.push(function () {
+			return self.run('npm prune', 'Running npm prune...'.magenta, '- npm prune done.'.magenta);
+		});
+
+		return function () {
+			return self.seq(promises);
+		};
 	},
-	npm: function () {
-		if (true === this.hasPackageJSON) {
-			var self = this;
-			return [
-				function () {
-					return self.run('npm install', 'Running npm install...'.magenta, 'Running npm install done.'.magenta);
-				},
-				function () {
-					return self.run('npm update', 'Running npm update...'.magenta, 'Running npm update done.'.magenta);
-				},
-				function () {
-					return self.run('npm prune', 'Running npm prune...'.magenta, 'Running npm prune done.'.magenta);
-				}
-			];
-		} else {
-			console.log("No package.json found, skipping npm calls.".underline.yellow);
-		}
-		return [];
+
+	bower: function (location) {
+		var self = this;
+		var promises = [
+			function () {
+				return self.goto(location, "bower");
+			}
+		];
+		commander.install && promises.push(function () {
+			return self.run('bower install', 'Running bower install...'.blue, '- bower install done.'.blue);
+		});
+		commander.update && promises.push(function () {
+			return self.run('bower update', 'Running bower update...'.blue, '- bower update done.'.blue);
+		});
+		commander.prune && promises.push(function () {
+			return self.run('bower prune', 'Running bower prune...'.blue, '- bower prune done.'.blue);
+		});
+
+		return function () {
+			return self.seq(promises);
+		};
 	},
+
 	all: function () {
-		this.checkNPM();
-		var promises = [];
-		if (program.bower) {
-			promises = promises.concat(this.bower());
+		var self = this;
+
+		var npmPromises = [];
+		if (commander.npm) {
+			this.npmLocations = this.find(process.cwd(), 'package.json', true);
+
+			if (!_.isEmpty(this.npmLocations)) {
+				_.forEach(this.npmLocations, function (location) {
+					npmPromises.push(self.npm(location));
+				});
+
+			} else {
+				console.log("No package.json found, skipping npm calls.".underline.yellow);
+			}
 		}
-		if (program.npm) {
-			promises = promises.concat(this.npm());
+
+		var bowerPromises = [];
+		if (commander.bower) {
+			this.bowerLocations = this.find(process.cwd(), 'bower.json');
+
+			if (!_.isEmpty(this.bowerLocations)) {
+				_.forEach(this.bowerLocations, function (location) {
+					bowerPromises.push(self.bower(location));
+				});
+
+			} else {
+				console.log("No bower.json found, skipping bower calls.".underline.yellow);
+			}
 		}
-		this.seq(promises).then(function (res) {
+
+		var promises = _.concat(npmPromises, bowerPromises);
+		self.seq(promises).then(function (res) {
 			console.log('Done.'.green);
 		});
 	}
