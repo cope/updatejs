9603af7cb38ba4aa4203918e419e6692e24330cc
Predrag Stojadinovic <predrag@stojadinovic.net>
2018-08-15 18:49:13 +0200

    fixing codacy issues

--- test/updatejs.test.js
+++ test/updatejs.test.js
@@ -1,14 +1,14 @@
-const _ = require('lodash');
+const _ = require("lodash");
 const expect = require("chai").expect;
 
 const updatejs = require("../lib/updatejs");
 
 describe("updatejs tests", function () {
-	it('updatejs should exist', function () {
+	it("updatejs should exist", function () {
 		expect(updatejs).to.exist;
 	});
 
-	it('updatejs.update should exist', function () {
+	it("updatejs.update should exist", function () {
 		expect(updatejs.update).to.exist;
 	});
 
